0c2dd9e35e3f400a38c9bfe51ed8a9c3f046b242
Lucas Adams <lucasadams@protonmail.com>
2025-01-13 08:48:33 +0000

    Create DTO struct between BL and UI

---
+++ focus_aid.BusinessLogic/DTOs/CountdownSessionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focus_aid.BusinessLogic.DTOs
+{
+    public struct CountdownSessionDTO
+    {
+        int Seconds;
+        string FeedbackMessage;
+        Instruction Instruction;
+    }
+}
