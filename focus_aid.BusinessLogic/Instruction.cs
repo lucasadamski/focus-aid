213c45c3fee9fa41484f5a957f42f6990ba6c311
Lucas Adams <lucasadams@protonmail.com>
2025-01-13 08:41:47 +0000

    Create instruction for communication between UI and Business Logic

---
+++ focus_aid.BusinessLogic/Instruction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focus_aid.BusinessLogic
+{
+    enum Instruction
+    {
+        None    = 0,
+        Stop    = 1,
+        Start   = 2,
+        Pause   = 3
+    }
+}
