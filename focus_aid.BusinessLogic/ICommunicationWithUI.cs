5c0fe62afdb6b60e301a725455540c4fe1ead38f
Lucas Adams <lucasadams@protonmail.com>
2025-01-18 10:25:46 +0000

    Communication with UI interface

---
+++ focus_aid.BusinessLogic/ICommunicationWithUI.cs
@@ -0,0 +1,15 @@
+﻿using focus_aid.BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focus_aid.BusinessLogic
+{
+    public interface ICommunicationWithUI
+    {
+        CountdownSessionDTO SendToUI();
+        bool ReceiveFromUI(CountdownSessionDTO countdownSessionDTO);
+    }
+}
