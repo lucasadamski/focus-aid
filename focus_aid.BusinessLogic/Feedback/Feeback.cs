7ea4296dcd5576c7dbc3d0016affa1ce7a8e78f9
Lucas Adams <lucasadams@protonmail.com>
2025-01-20 11:47:08 +0000

    Created Feedback class and interface

---
+++ focus_aid.BusinessLogic/Feedback/Feeback.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focus_aid.BusinessLogic.Feedback
+{
+    public class Feeback : IFeedback
+    {
+        public string? Message { get; set; }
+        public bool Success { get; set; }
+
+        public bool MarkSuccess(bool successful)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadFeedback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WriteFeedback(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ focus_aid.BusinessLogic/Feedback/IFeedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace focus_aid.BusinessLogic.Feedback
+{
+    public interface IFeedback
+    {
+        public bool WriteFeedback(string message);
+        public string ReadFeedback();
+        public bool MarkSuccess(bool successful);
+
+    }
+}
