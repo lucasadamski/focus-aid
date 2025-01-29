606e45a4f248bc50d55c7d1cc3bb582784c571e8
Lucas Adams <lucasadams@protonmail.com>
2025-01-29 20:23:57 +0000

    Removed unit tests

--- focus_aid.BusinessLogicTests/CountdownSessionManagerUnitTests.cs
+++ focus_aid.BusinessLogicTests/CountdownSessionManagerUnitTests.cs
@@ -9,57 +9,7 @@ namespace focus_aid.BusinessLogicTests
     public class CountdownSessionManagerUnitTests
     {
         
-        [Test]
-        public void Start_InitializesWithoutSeconds_ReturnsFalse()
-        {
-            // Arrange
-            var csm = new CoutdownByDelegatingToUi();
-
-            // Act
-            bool result = csm.Start();
-
-            // Assert
-            result.Should().Be(false); 
-        }
-
-        [Test]
-        public void Start_InitializesWithNegativeSeconds_ReturnsFalse()
-        {
-            // Arrange
-            var csm = new CoutdownByDelegatingToUi(-73);
-
-            // Act
-            bool result = csm.Start();
-
-            // Assert
-            result.Should().Be(false);
-        }
-
-        [Test]
-        public void Start_InitializesWithZeroSeconds_ReturnsFalse()
-        {
-            // Arrange
-            var csm = new CoutdownByDelegatingToUi(0);
-
-            // Act
-            bool result = csm.Start();
-
-            // Assert
-            result.Should().Be(false);
-        }
-
-        [Test]
-        public void Start_InitializesWithPositiveSeconds_ReturnsTrue()
-        {
-            // Arrange
-            var csm = new CoutdownByDelegatingToUi(3600);
-
-            // Act
-            bool result = csm.Start();
-
-            // Assert
-            result.Should().Be(true);
-        }
+      
 
     }
 }
